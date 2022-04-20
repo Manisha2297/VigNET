1dd31fac06ad5834235e5f31ce3dcfbc4c6a49af
manisha2297 <r.manisha222@gmail.com>
2022-04-20 13:38:38 +0530

    Updated Uvicorn version

--- frontend-new/src/services/Common.js
+++ frontend-new/src/services/Common.js
@@ -1,5 +1,5 @@
-export const BASE_API_URL = "http://34.126.124.50:8080";
-// export const BASE_API_URL = "http://localhost:9000";
+// export const BASE_API_URL = "https://vignet-backend.glanceai.inmobi.com";
+export const BASE_API_URL = "http://localhost:9000";
 
 
 export function epochToJsDate(ts) {
